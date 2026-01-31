e6f0396dd7d110747186eea6465cc2bf09186da2
TheSabotender <admin@sabotender.com>
2026-01-31 19:43:19 +0100

    Add minimum height to player camera target

--- Assets/Scripts/Player/PlayerCameraTarget.cs
+++ Assets/Scripts/Player/PlayerCameraTarget.cs
@@ -9,6 +9,10 @@ public class PlayerCameraTarget : MonoBehaviour
 
     [SerializeField] private float maxDistanceFromPlayer;
 
+    [SerializeField] private float minimumHeight = 0f;
+
+    [SerializeField] private float minimumHeightSlowRange = 1f;
+
     private void Update()
     {
         var position = transform.position;
@@ -23,7 +27,25 @@ public class PlayerCameraTarget : MonoBehaviour
             position = Vector3.MoveTowards(position, newPos, maxDistanceFromPlayer);
         }
 
+        position.y = ApplyMinimumHeight(position.y);
         position.z = 0;
         cameraTarget.position = position;
+    }
+
+    private float ApplyMinimumHeight(float height)
+    {
+        if (height <= minimumHeight)
+        {
+            return minimumHeight;
+        }
+
+        if (minimumHeightSlowRange <= 0f || height >= minimumHeight + minimumHeightSlowRange)
+        {
+            return height;
+        }
+
+        var t = Mathf.InverseLerp(minimumHeight, minimumHeight + minimumHeightSlowRange, height);
+        var eased = Mathf.SmoothStep(0f, 1f, t);
+        return Mathf.Lerp(minimumHeight, minimumHeight + minimumHeightSlowRange, eased);
     }
 }
